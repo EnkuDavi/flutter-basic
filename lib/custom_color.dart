56328fc7fd7c54d65039e9579a3c21aa529d70e6
enkudavi <ekopur05@gmail.com>
2021-12-08 16:53:01 +0700

    modified color

---
+++ lib/custom_color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color darkBlue = Color(0XFF2c2c54);
--- lib/font-style.dart
+++ lib/font-style.dart
@@ -1,4 +1,5 @@
+import 'package:cashy_app/custom_color.dart';
 import 'package:flutter/material.dart';
 
 TextStyle MainHeader =
-    TextStyle(fontSize: 24, color: Colors.black38, fontFamily: 'Poppins');
+    TextStyle(fontSize: 24, color: darkBlue, fontFamily: 'Poppins');
